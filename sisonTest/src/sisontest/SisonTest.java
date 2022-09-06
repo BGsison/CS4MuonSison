d86d707ae420eda2223fc2bacf0907f8f9f5b60f
BGsison <97590804+BGsison@users.noreply.github.com>
2022-09-06 08:58:24 +0800

    Add files via upload

---
+++ sisonTest/src/sisontest/SisonTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisontest;
+
+/**
+ *
+ * @author MUON
+ */
+
+ import java.util.Scanner;
+public class SisonTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        //this is still a draft sorry ill submit 
+        //a final document when i get home
+        System.out.println( "Hello World!" );
+        class character {
+            character Luz = new character();
+            String name1 = "Luz";
+            char track1 = 'J';
+            short age1 = ;
+            
+            character Willow = new character();
+            
+            String name2;
+            char track2;
+            short age2;
+            
+            character Gus = new character();
+            String name3;
+            char track3;
+            short age3;
+        }
+    }
+}
